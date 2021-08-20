48925af5c243a061ae8e9540dcbc4d35403b39a2
jjliewie ^^ <52589420+jjliewie@users.noreply.github.com>
2021-08-20 15:35:19 +0800

    Time: 56 ms (78.79%), Space: 14.2 MB (77.76%) - LeetHub

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -2,16 +2,4 @@ import math
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        number = str(x)
-        
-        if x == 0:
-            return True
-        
-        if x < 0:
-            return False
-        
-        for i in range(math.floor(len(number)/2)):
-            if number[i] != number[len(number)-i-1]:
-                return False
-        
-        return True
+        return str(x)[::-1]==str(x)
