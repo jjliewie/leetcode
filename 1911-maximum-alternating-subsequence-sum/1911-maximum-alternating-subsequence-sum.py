fe4433628d210d3f2bd2ac5e4289c8eeacb59b03
jjliewie ^^ <52589420+jjliewie@users.noreply.github.com>
2022-02-10 18:38:31 +0800

    Time: 1830 ms (51.78%), Space: 28.4 MB (93.91%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,7 +1,10 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        ans = 0
-        for i in range(len(nums)-1):
-            ans += max(0, nums[i+1] - nums[i])
+        
+        return sum([max(0, nums[i+1] - nums[i]) for i in range(len(nums)-1)]) + nums[0]
+        
+#         ans = 0
+#         for i in range(len(nums)-1):
+#             ans += max(0, nums[i+1] - nums[i])
 
-        return ans + nums[0]
+#         return ans + nums[0]
