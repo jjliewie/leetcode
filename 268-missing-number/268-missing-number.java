737de0ab5675cc061baa07f72e025aa5a39a71ac
jjliewie ^^ <52589420+jjliewie@users.noreply.github.com>
2022-04-02 10:23:59 +0800

    Time: 0 ms (100.00%), Space: 52 MB (11.26%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    public int missingNumber(int[] nums) {
+        
+        int sum1 = 0;
+        int sum2 = 0;
+        
+        for(int i = 0; i <= nums.length; i++){
+            sum1 += i;
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            sum2 += nums[i];
+        }
+        
+        return sum1-sum2;
+        
+    }
+}
